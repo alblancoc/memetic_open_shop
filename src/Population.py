b40e146917b0c7c866c410c1055afa9b9912a619
ANGIE <ANGIE@192.168.0.22>
2016-05-08 22:10:40 -0500

    construccion heuristica

--- src/Population.py
+++ src/Population.py
@@ -3,15 +3,45 @@ Created on Apr 26, 2016
 
 @author: carlosandressierra
 '''
+from random import shuffle
+from math import ceil
+from math import floor
+#class Population(object):
+   # '''
+   # classdocs
+    
+   # '''
+    
+maquinas = 4
+trabajos = 4
+#poblacion = [(i + 1) for i in range(maquinas * trabajos)] #Definir poblacion inicial
+#shuffle(poblacion)
 
-class Population(object):
-    '''
-    classdocs
-    '''
+tamano = 2
+poblacion = []
 
+for x in range(tamano):
+    individuo = [(i + 1) for i in range(maquinas * trabajos)]
+    shuffle(individuo)
+    poblacion.append( individuo )
 
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
+print poblacion
+
+def fila(m):
+    return ceil(m/maquinas)
+    
+def columna(n):
+    c = n % maquinas
+    if c == 0:
+        c = maquinas
+    
+    return c 
+ 
+for x in range(tamano):
+    poblacion[x]
+
+   # def __init__(self, params):
+    #    '''
+     #   Constructor
+      #  '''
         
